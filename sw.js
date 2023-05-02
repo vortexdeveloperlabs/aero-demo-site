111054f26b91d9533b86234de82da42490d42da9
Ryan Wilson <92859295+MovByte@users.noreply.github.com>
2023-05-02 18:43:17 -0400

    Improved error handling for aero

--- sw.js
+++ sw.js
@@ -3,8 +3,50 @@ import "./aero/init.js";
 
 self.addEventListener("install", () => self.skipWaiting());
 
+function wrapLink(link) {
+	console.log(link);
+	link = link.replace(/:\d+:\d+$/g, "");
+
+	return `<a href="${link}">${link}</a>`;
+}
+function fmtErr(stack) {
+	return (
+		stack
+			// Put locations on a new line and tab
+			.split("at ")
+			.map(loc => loc.replace(/^(http:|https:).+/gm, m => wrapLink(m)))
+			.join("<br>&emsp;")
+			// Bold the error
+			.replace(/(^[^:]*)(?:: )([^:]*)/g, "<b>$1</b>: <i>$2</i>")
+			// Format the links
+			.replace(/\(([^()]*)\)/gms, (_m, g1) => `(${wrapLink(g1)})`)
+	);
+}
+
 self.addEventListener("fetch", async event =>
 	event.respondWith(
-		handle(event).catch(err => new Response(err.stack, { status: 500 }))
+		handle(event).catch(
+			err =>
+				new Response(
+					`
+<!DOCTYPE html>
+<html>
+	<body>
+		<style>
+			body {
+				font-family: arial, sans-serif;
+			}
+			#err {
+				font-family: monospace;
+			}
+		</style>
+		<h1 id="title" style="color: red">Aero Bug</h1>
+		<p id="err">${fmtErr(err.stack)}<p>
+	</body>
+</html>
+			`,
+					{ headers: { "content-type": "text/html" }, status: 500 }
+				)
+		)
 	)
 );
